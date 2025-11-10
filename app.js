14552a0ca0bf66ef74607e4c0fc645c330554302
Krysiuuuuu <alicja.kubacka@gmail.com>
2025-11-10 14:55:13 +0100

    Add app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+document.getElementById("loginBtn").addEventListener("click", function () {
+  const password = document.getElementById("passwordInput").value;
+  const output = document.getElementById("output");
+
+  switch (password) {
+    case "G(*4#":
+      output.textContent = "Logged in";
+      break;
+    case ";UUNX":
+      output.textContent = "|\\logedd in w";
+      break;
+    case "vsu6r4":
+      output.textContent = "reLOGEDD IN~1n";
+      break;
+    case "rwga?nh":
+      output.textContent = "555. They are.";
+      break;
+    default:
+      output.textContent = "Invalid password.";
+  }
+});
